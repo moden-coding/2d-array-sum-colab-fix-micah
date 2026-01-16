b574c50094fac06959022d44ac5520855406d73a
MicahGann <micahgann3@gmail.com>
2026-01-16 12:30:38 -0500

    done

--- src/App.java
+++ src/App.java
@@ -9,7 +9,13 @@ public class App {
 	}
 	
 	public static int sum2D(int[][] nums){
-		return -1;
+		int sum = 0;
+		for(int i = 0; i < nums.length; i++){
+			for(int j = 0; j < nums[i].length; j++){
+				sum += nums[i][j];
+			}
+		}
+		return sum;
 	}
 	
 }
